5e7616a0b3190c984b928edc6b639bbe5a87ff82
Paul H Choi <pchoi@getsidecar.com>
2021-12-01 23:42:55 -0500

    day 1 - part two

--- src/day1.py
+++ src/day1.py
@@ -1,13 +1,20 @@
 input_file = 'input/day1.txt'
 
 file_lines = open(input_file, 'r').readlines()
+values = []
+for value in file_lines:
+  current_value = int(value.strip())
+  values.append(current_value)
+
+sums = values + values[:3]
+window_sums = [sum(sums[i:i+3]) for i in range(len(values))]
 
 count = 0
 previous_value = 0
 increased = 0
 
-for value in file_lines:
-  current_value = int(value.strip())
+for value in window_sums:
+  current_value = value
   if previous_value < current_value and count != 0:
     increased += 1
 
