ef606ca3a5a1f9a975ef1ba31173f2321ffee036
Paul H Choi <pchoi@getsidecar.com>
2021-12-02 00:11:30 -0500

    day 2 - part two

--- src/day2.py
+++ src/day2.py
@@ -2,19 +2,21 @@ input_file = 'input/day2.txt'
 
 horiz = 0
 vert = 0
+aim = 0
 
 file_lines = open(input_file, 'r').readlines()
 for value in file_lines:
   direction, amount = value.split()
   amount = int(amount.strip())
 
-  if direction == "forward":
-    horiz += amount
+  if direction == "down":
+    aim += amount
 
   if direction == "up":
-    vert -= amount
+    aim -= amount
 
-  if direction == "down":
-    vert += amount
+  if direction == "forward":
+    horiz += amount
+    vert += aim * amount
 
 print (horiz * vert)
